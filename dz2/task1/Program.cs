befa04d3a1c488006005efaa36b684d326ce1ad2
Alexey <shiry-1980@mail.ru>
2022-12-08 19:09:04 +0300

    task 1

---
+++ dz2/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+ Console.WriteLine("Введите трёхзначное число ");
+       
+        int a = Convert.ToInt32(Console.ReadLine());
+         
+        int n = ((a/10) % 10);
+         
+        
+         	
+        Console.WriteLine($"Вторая цифра {n}");
