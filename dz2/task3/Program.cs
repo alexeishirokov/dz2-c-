33560ab19f41f2cc54c6065def2d4104cfc563fa
Alexey <shiry-1980@mail.ru>
2022-12-08 19:11:23 +0300

    task 3

---
+++ dz2/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число недели");
+      
+       int a = int.Parse(Console.ReadLine());
+       
+         if(a > 5 && a < 8){
+         	
+         	Console.WriteLine("Это выходной");
+         }
+         
+         else{
+         	
+         	Console.WriteLine("Это рабочий день");
+         }
