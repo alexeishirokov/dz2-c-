d2344ed6c42aae6e9f0c0e3e060a69f55e38599e
Alexey <shiry-1980@mail.ru>
2022-12-08 19:10:37 +0300

    task 2

---
+++ dz2/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+ Console.WriteLine("Введите число: ");
+       
+       int a = int.Parse(Console.ReadLine());
+        
+       if(a <= 99){
+        	
+        	Console.WriteLine($"в числе {a} нет третьей цифры");
+
+        	Console.WriteLine("Введите другое число: ");
+
+       }
+        
+        	if(a > 99 && a < 1000){
+        		
+        		Console.WriteLine($"Третья цифра {a % 10}");
+        	
+            }
+    
+        	else{
+                while(a > 999)
+             	
+             	a = a / 10;
+             	
+             	Console.WriteLine($"Третья цифра  {a % 10}");
+                
+            }
+       
+    
+       
