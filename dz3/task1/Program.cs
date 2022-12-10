825aebad4de8f898b942cef6b0d3b4969b707453
Alexey <shiry-1980@mail.ru>
2022-12-10 19:25:28 +0300

    19

---
+++ dz3/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿  Console.Clear();
+
+   Console.Write("Введите пятизначное число: ");
+   
+   string a = Console.ReadLine();
+
+    int n = a.Length;
+
+if (n == 5)
+{
+  if (a[0] == a[4] && a[1] == a[3])
+    {
+        Console.WriteLine("Это число является палиндромом");
+    }
+    else
+       {
+        Console.WriteLine("Это число не является палиндромом");
+      }
+        }
+          else
+     {
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
+    
