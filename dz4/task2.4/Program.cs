7385db6f73a1ac43b3e64c8af146b894a6492830
Alexey <shiry-1980@mail.ru>
2022-12-11 20:06:55 +0300

    27

---
+++ dz4/task2.4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+  int sum = 0;
+
+   while(a > 0){
+
+    sum = sum + a % 10;
+    a /=10;
+   }
+
+    Console.WriteLine(sum);
