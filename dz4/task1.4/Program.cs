da2fb866458e5239f0564832d975aed6471f5ab0
Alexey <shiry-1980@mail.ru>
2022-12-11 18:11:52 +0300

    25

---
+++ dz4/task1.4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+   Console.Write("Введите первое число: ");
+
+    int a = Convert.ToInt32(Console.ReadLine());
+
+   Console.Write("Введите второе число: ");
+
+   int b = Convert.ToInt32(Console.ReadLine());
+
+   int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
