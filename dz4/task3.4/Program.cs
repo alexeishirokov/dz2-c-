9b261f7af516653bcb7971c137206f6a9416f206
Alexey <shiry-1980@mail.ru>
2022-12-15 11:11:37 +0300

    29

---
+++ dz4/task3.4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество чисел в массиве: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = new int[a];
+Console.WriteLine("Введите любые числа, количество которых равно вашему массиву: ");
+
+ for (int i = 0; i < a; i++){
+//Console.Write("Введите любые числа, количество которых равно вашему массиву: ");
+ array [i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{String.Join(", " ,array)}]");
