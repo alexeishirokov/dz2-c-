26cea0ddf82809e9e600c181d1ab02808bfb0836
Alexey <shiry-1980@mail.ru>
2022-12-15 14:37:35 +0300

    36

---
+++ dz5/task2.5/Program.cs
@@ -0,0 +1,25 @@
+﻿void inArr(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 10); 
+}
+
+
+void sumUneven(int[] array){
+
+int sumUneven = 0;
+for (int i = 1; i < array.Length; i+=2){
+
+   sumUneven = sumUneven + (array[i]);
+}
+      Console.WriteLine($"Сумма чисел на нечётных позициях равна: {sumUneven}");
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+inArr(array);
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+sumUneven(array);
