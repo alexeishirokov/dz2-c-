a332780d3e2472dd6de57c71c1d4d9c0ce79ea58
Alexey <shiry-1980@mail.ru>
2022-12-15 13:11:59 +0300

    34

---
+++ dz5/task1.5/Program.cs
@@ -0,0 +1,26 @@
+﻿ void even(int[] array)
+{
+int even = 0;
+for (int j = 0; j < array.Length; j++)
+{
+if (array[j] % 2 !=0);
+
+else
+    even++;
+}
+Console.WriteLine($"Сумма чётных чисел равна {even}");
+    
+} 
+    Console.Clear();
+
+    int quantity = new Random().Next(10, 100);
+
+    int[] array =  new int[quantity];
+
+    for(int i = 0; i < array.Length; i++)
+
+      array[i] = new Random().Next(100, 1000);
+    
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+
+     even(array);
