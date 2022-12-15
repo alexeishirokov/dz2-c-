1b653b425129304e722e955acdfd9bc5e159ad1f
Alexey <shiry-1980@mail.ru>
2022-12-15 18:37:03 +0300

    38

---
+++ dz5/task3.5/Program.cs
@@ -0,0 +1,30 @@
+﻿void InArr(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+     array[i] = new Random().Next(10, 100);
+}
+
+void sumResult(int[] array)
+{
+    int max = 0;
+    int min = array[0];
+  for (int i = 0; i < array.Length; i++){
+
+    if(array[i] > max){
+        max = array[i];
+    }
+    if(array[i] < min){
+        min = array[i];
+    }  
+}
+    Console.WriteLine($"max и min: {max} и {min}");
+    Console.WriteLine($"Разница между ними: {max - min}");
+   
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InArr(array);
+Console.WriteLine($" [{string.Join(", ", array)}]");
+sumResult(array);
