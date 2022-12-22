5d7720993afa0e6630adf10aa6ef0cc0bedb34ef
Alexey <shiry-1980@mail.ru>
2022-12-22 14:23:39 +0300

    41

---
+++ dz6/task1.6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите числа массива через пробел: ");
+int[] n = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int sum = 0;
+for (int i = 0; i < n.Length; i++)
+{
+    if (n[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine($"количество значений больше 0 = {sum}");
